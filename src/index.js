09bdc1354291e051c1fdfc617e889c4b8737e29f
jacobrees <jacob.rees97@yahoo.com>
2021-08-08 00:08:50 +0100

    Add homebutton and functionality to home button to app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './scss/styles.scss';
+
+const contentContainer = document.querySelector('.content');
+
+const loadHomePage = () => {
+  contentContainer.innerHTML = `<div class="search-content">
+            <h2 class="search-title">Search By Location</h2>
+            <form class="search-form" action="#"><input class="search-box" type="text"
+                    placeholder="Search by city/town">
+                <button class="search-btn" type="button">Search</button>
+            </form>
+
+        </div>`;
+};
+
+const homeBtn = document.querySelector('.home-btn');
+
+homeBtn.addEventListener('click', () => {
+  loadHomePage();
+});
