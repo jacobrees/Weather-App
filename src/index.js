9ff740e168a2d97ae8ec91ef4303f14be688bfd4
jacobrees <jacob.rees97@yahoo.com>
2021-08-08 03:02:04 +0100

    Add functionality to search when user pushes enter btn

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
 import './scss/styles.scss';
 
+const contentContainer = document.querySelector('.content');
+
 const setSearchBtn = () => {
   const searchBtn = document.querySelector('.search-btn');
+  const searchBox = document.querySelector('.search-box');
 
   searchBtn.addEventListener('click', (e) => {
-    const searchBox = document.querySelector('.search-box');
     e.preventDefault();
     console.log(searchBox.value);
+    contentContainer.innerHTML = ` <div class="search-results-content">
+    <div class="country-result">
+        <div class="country-name-container">
+            <img class="search-country-flag" src="https://flagcdn.com/za.svg" alt="South Africa">
+            <h3 class="country-name">South Africa</h3>
+        </div>
+        <div class="city-name-container">
+            <h4>City/Town: Swansea</h4>
+        </div>
+        <div class="latitude-container">
+            <h4>Latitude: S 54° 16' 52''</h4>
+        </div>
+        <div class="logatude-container">
+            <h4>Longatude: W 36° 30' 33''</h4>
+        </div>
+    </div>
+</div>`;
+  });
+  searchBox.addEventListener('keypress', (e) => {
+    if (e.keyCode === 13) {
+      searchBtn.click();
+    }
   });
 };
-
-const contentContainer = document.querySelector('.content');
 
 const loadHomePage = () => {
   contentContainer.innerHTML = `<div class="search-content">
