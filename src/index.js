f131f88b2e0fc5ced8c328c87834fae59b16199d
jacobrees <jacob.rees97@yahoo.com>
2021-08-09 01:01:22 +0100

    Create loadSearchPage function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ import './scss/styles.scss';
 
 const contentContainer = document.querySelector('.content');
 
+const loadSearchPage = () => {
+  contentContainer.innerHTML = ` <div class="search-results-content">
+  <div class="country-result">
+      <div class="country-name-container">
+          <img class="search-country-flag" src="https://flagcdn.com/sh.svg" alt="Saint Helena, Ascension and Tristan da Cunha">
+          <h3 class="country-name">Saint Helena, Ascension and Tristan da Cunha</h3>
+      </div>
+      <div class="city-name-container">
+          <h4>City/Town: Swansea</h4>
+      </div>
+      <div class="latitude-container">
+          <h4>Latitude: S 54° 16' 52''</h4>
+      </div>
+      <div class="longitude-container">
+          <h4>Longitude: W 36° 30' 33''</h4>
+      </div>
+  </div>
+  </div>`;
+};
+
 const setSearchBtn = () => {
   const searchBtn = document.querySelector('.search-btn');
   const searchBox = document.querySelector('.search-box');
@@ -9,38 +29,7 @@ const setSearchBtn = () => {
   searchBtn.addEventListener('click', (e) => {
     e.preventDefault();
     console.log(searchBox.value);
-    contentContainer.innerHTML = ` <div class="search-results-content">
-    <div class="country-result">
-        <div class="country-name-container">
-            <img class="search-country-flag" src="https://flagcdn.com/sh.svg" alt="Saint Helena, Ascension and Tristan da Cunha">
-            <h3 class="country-name">Saint Helena, Ascension and Tristan da Cunha</h3>
-        </div>
-        <div class="city-name-container">
-            <h4>City/Town: Swansea</h4>
-        </div>
-        <div class="latitude-container">
-            <h4>Latitude: S 54° 16' 52''</h4>
-        </div>
-        <div class="longitude-container">
-            <h4>Longitude: W 36° 30' 33''</h4>
-        </div>
-    </div>
-    <div class="country-result">
-    <div class="country-name-container">
-        <img class="search-country-flag" src="https://flagcdn.com/sa.svg" alt="South Africa">
-        <h3 class="country-name">South Africa</h3>
-    </div>
-    <div class="city-name-container">
-        <h4>City/Town: Swansea</h4>
-    </div>
-    <div class="latitude-container">
-        <h4>Latitude: S 54° 16' 52''</h4>
-    </div>
-    <div class="longitude-container">
-        <h4>Longitude: W 36° 30' 33''</h4>
-    </div>
-</div>
-</div>`;
+    loadSearchPage();
   });
   searchBox.addEventListener('keypress', (e) => {
     if (e.keyCode === 13) {
