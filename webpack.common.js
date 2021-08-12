049c7e8b28bb64657e314135bcc99f2895a11d80
jacobrees <jacob.rees97@yahoo.com>
2021-08-12 02:29:19 +0100

    Add svg assets and functionality to display an icon based on the weather

--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,15 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  plugins: [new HtmlWebpackPlugin({ template: './src/template.html', minify: false })],
+  plugins: [new HtmlWebpackPlugin({ template: 'src/template.html', minify: false }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/assets/imgs', to: 'assets/imgs' },
+      ],
+    }),
+  ],
   module: {
     rules: [
       {
