93d8fa45fd9f364e680ef89a32652186325af96e
jacobrees <jacob.rees97@yahoo.com>
2021-08-12 06:47:06 +0100

    Update arrow icon for wind direction

--- src/javascript/display.js
+++ src/javascript/display.js
@@ -22,7 +22,7 @@ const loadWeatherPage = (weather) => {
                     <h2 class="weather-winds-speed">Speed: ${weather.wind.speed} mph</h2>
                     <h2 class="weather-winds-direction">
                     Direction: ${degreeToDirection(weather.wind.deg)}
-                    <svg class="wind-direction-svg" style="transform: rotate(${weather.wind.deg}deg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 7.58l5.995 5.988-1.416 1.414-4.579-4.574-4.59 4.574-1.416-1.414 6.006-5.988z"/></svg>
+                    <svg class="wind-direction-svg" style="transform: rotate(${weather.wind.deg}deg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M7 11h-6l11-11 11 11h-6v13h-10z"/></svg>
                     </h2>
                     <div class="humidity-cloudiness-container">
                         <div class="humidity-container">
